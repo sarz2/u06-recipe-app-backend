fb3438c8368fbbf4f340f104320e301c76230c25
sarz2 <sara.abou@chasacademy.se>
2022-03-30 14:04:12 +0200

    add logic to UserController

---
+++ project/u06-backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Respond;
+use Illuminate\Support\Facades\Hash;
+
+
+
+class UserController extends Controller
+{
+    public function register(Request $request){
+
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+
+        ]);
+
+        $token = $user->createToken('recipeapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return $response($response, 201);
+
+    }
+
+    public function login(Request $request){ 
+        $fields = $request->validate([
+        'email' => 'required|string',
+        'password' => 'required|string'
+    ]);
+
+    //check email
+
+    $user = User::where('email', $fields['email'])->first();
+
+    //check password
+    if(!$user || !Hash::check($fields['password'], $user->password)){
+        return response([
+            'message' => 'Wrong password'
+        ], 401);
+    }
+
+    $token = $user->createToken('recipeapptoken')->plainTextToken;
+
+    $response = [
+        'user' => $user,
+        'token' => $token
+    ];
+
+    return response($response, 201);
+
+
+}
+
+    public function logout(Request $request){
+        auth()->user()->tokens()->delete();
+
+        return response([
+            'message'=> 'logged out'
+        ], 401);
+    }
+}
--- project/u06-backend/routes/api.php
+++ project/u06-backend/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,10 @@ use App\Http\Controllers\RecipeController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
